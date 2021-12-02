9f86be648562571eddb8479396cef88c91275a98
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2021-12-02 20:33:09 +0100

    Use case classes

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -5,7 +5,11 @@ import scala.io.Source
 object Day01 {
   def main(args: Array[String]): Unit = {
     val depths = Util.getLines("day01") map { _.toInt }
-    val windows = Range(2, depths.size).map { idx => depths(idx - 2) + depths(idx - 1) + depths(idx)}
-    println(Range(1, windows.size).map({ (idx: Int) => if windows(idx - 1) < windows(idx) then 1 else 0 }).sum)
+    val windows = Range(2, depths.size).map { idx =>
+      depths(idx - 2) + depths(idx - 1) + depths(idx)
+    }
+    println(Range(1, windows.size).map({ (idx: Int) =>
+      if windows(idx - 1) < windows(idx) then 1 else 0
+    }).sum)
   }
 }
--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -1,17 +1,21 @@
 package fr.nate
 
+case class Instruction(verb: String, n: Int)
+
+case class Position(x: Int, y: Int, aim: Int)
+
 object Day02 {
   def main(args: Array[String]): Unit = {
     val lines = Util.getLines("day02")
     val instructions = lines map { line =>
       val words = line split " "
-      (words(0), words(1).toInt)
+      Instruction(words(0), words(1).toInt)
     }
-    val arrival = instructions.foldLeft((0, 0, 0)) { (pos, instr) =>
+    val arrival = instructions.foldLeft(Position(0, 0, 0)) { (pos, instr) =>
       instr match {
-        case ("forward", n) => (pos._1 + n, pos._2 + (n * pos._3), pos._3)
-        case ("up", n) => (pos._1, pos._2, pos._3 - n)
-        case ("down", n) => (pos._1, pos._2, pos._3 + n)
+        case Instruction("forward", n) => Position(pos.x + n, pos.y + (n * pos.aim), pos.aim)
+        case Instruction("up", n) => Position(pos.x, pos.y, pos.aim - n)
+        case Instruction("down", n) => Position(pos.x, pos.y, pos.aim + n)
       }
     }
     println(arrival)
